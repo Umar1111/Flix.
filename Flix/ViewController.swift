66694331a5c8ed807e1c3ea2880d792b37ae97dd
Umar Arshad <51463944+Umar1111@users.noreply.github.com>
2022-09-20 00:54:24 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Sohail Arshad on 9/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
